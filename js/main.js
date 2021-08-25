09f539a3ef48e49220059414fde91f6213e25513
Lolu Adelore <adeloreisaac@gmail.com>
2021-08-25 16:52:53 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function makeEnquiries() {
+    window.location.href = 'homes/contact-us.html'
+}
+
+function enterpriseHomes() {
+    window.location.href = 'https://www.theenterprisehomes.com/homes', '_blank'
+}
+
+function enterpriseHotel() {
+    window.location.href = 'http://www.theenterprisehotel.com', '_blank'
+}
